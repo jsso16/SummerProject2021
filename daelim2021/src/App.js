a014716bb4cec67a75bf48346f079cbd40efdf37
jsso16 <wjsthwls24@daum.net>
2021-07-21 09:59:51 +0900

    Add React files

---
+++ daelim2021/src/App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header'
+import Contents from './components/contents'
+import Footer from './components/footer';
+
+function App() {
+  const content_data = [{
+    img: "../images/main02.jpeg",
+    isNow: true,
+    title: "[대림대학교 ㅣ 대...",
+    title_on: "대림대학교 ㅣ 대림대 공식 홍보영상(..."
+  },{
+    img: "../images/main03.jpeg",
+    isNow: false,
+    title: "대림대학교 컴퓨터...",
+    title_on: "대림대학교 컴퓨터정보학부를 소개합..."
+  },{
+    img: "../images/main04.jpeg",
+    isNow: false,
+    title: "[대림대학교 ㅣ 대...",
+    title_on: "대림대학교 ㅣ 대림대 공식 홍보영상(..."
+  },{
+    img: "../images/main05.jpeg",
+    isNow: false,
+    title: "[대림대학교 ㅣ 대...",
+    title_on: "대림대학교 ㅣ 대림대 공식 홍보영상(..."
+  },{
+    img: "../images/main06.jpeg",
+    isNow: false,
+    title: "[대림대학교 ㅣ 대...",
+    title_on: "대림대학교 ㅣ 대림대 공식 홍보영상(..."
+  },{
+    img: "../images/main07.jpeg",
+    isNow: false,
+    title: "[대림대학교 ㅣ 대...",
+    title_on: "대림대학교 ㅣ 대림대 공식 홍보영상(..."
+  },{
+    img: "../images/main08.jpeg",
+    isNow: false,
+    title: "[대림대학교 ㅣ 대...",
+    title_on: "대림대학교 ㅣ 대림대 공식 홍보영상(..."
+  }]
+
+  const menu_data = [{
+    menu: "입학 홈페이지"
+  },{
+    menu: "합격예측서비스"
+  },{
+    menu: "대림대학교 홈페이지"
+  },{
+    menu: "로그인"
+  }]
+
+  return (
+    <div id="wrap">
+      <Header></Header>
+      <Contents content_data={content_data} menu_data={menu_data}></Contents>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
