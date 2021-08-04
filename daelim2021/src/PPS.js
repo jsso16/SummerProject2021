6a9d25b3bef6afabc5231a0bce42f94718bfa8af
jsso16 <wjsthwls24@daum.net>
2021-08-04 11:46:48 +0900

    Add react files

---
+++ daelim2021/src/PPS.js
@@ -0,0 +1,48 @@
+function PPS() {
+  return (
+    <div id="wrap">
+      <header>
+        <div class="top">
+          <div class="inner">
+            <h1 class="logo">
+              <a href="#">
+                <img src="../images/PPS/logo.png" alt="대림대학교" />
+              </a>
+            </h1>
+            <button class="btn">MENU</button>
+          </div>
+        </div>
+      </header>
+      <section id="contents">
+        <h2 class="title">
+          <img src="../images/PPS/title.png" alt="Ace Delim" />
+          <span class="txt">최고수준의 대한민국 대표<br>고등직업교육기관으로 도약!</span>
+        </h2>
+        <div class="cst">
+          <div class="box left">
+            <a href="#" class="open_right">
+              <img src="../images/PPS/right_btn.png" alt="button" />
+            </a>
+            <div class="box_txt">
+              <dl>
+                <dt>수시합격예측</dt>
+              </dl> 
+            </div>
+          </div>
+          <div class="box right">
+            <a href="#" class="open_left">
+              <img src="../images/PPS/left_btn.png" alt="button" />
+            </a>
+            <div class="box_txt">
+              <dl>
+                <dt>정시합격예측</dt>
+              </dl> 
+            </div>
+          </div>
+        </div>
+      </section>
+    </div> 
+  )
+}
+
+export default PPS
